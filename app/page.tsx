7ed9f289eacb5b1da3c56f2aca9058b04213062f
Mark Estareja <mark.estareja.me@gmail.com>
2026-09-04 08:32:45 +0800

    Remove full height of date

--- app/page.tsx
+++ app/page.tsx
@@ -316,7 +316,7 @@ export default function Home() {
           6:00 pm - 9:00 pm
         </p>
 
-        <div className="relative w-full h-full">
+        <div className="relative w-full">
           <Image
             src="/date.webp"
             alt="date"
